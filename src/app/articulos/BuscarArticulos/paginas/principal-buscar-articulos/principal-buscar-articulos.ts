47baaf017760d7645f3a116d53ee1a7b6274c41f
ManuelFas <manuelfasruiz@gmail.com>
2026-03-10 14:53:54 +0100

    Show the Websites done

--- src/app/articulos/BuscarArticulos/paginas/principal-buscar-articulos/principal-buscar-articulos.ts
+++ src/app/articulos/BuscarArticulos/paginas/principal-buscar-articulos/principal-buscar-articulos.ts
@@ -2,6 +2,8 @@ import { Component, inject, signal } from '@angular/core';
 import { ArxivService } from '../../servicios/arxiv-service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ArxivOut, ArxivSuccess, ResultadoConsultaArXiv } from '../../interfaces/arxiv-input';
+
 @Component({
   selector: 'app-principal-buscar-articulos',
   imports: [FormsModule, CommonModule],
@@ -9,13 +11,16 @@ import { CommonModule } from '@angular/common';
   styleUrl: './principal-buscar-articulos.css',
 })
 export class PrincipalBuscarArticulos {
+  
   _ArxivService = inject(ArxivService)
   query = ''
-  resultado = signal<string | null> (null)
+  resultado = signal<ResultadoConsultaArXiv | null> (null)
 
   buscarArticulo(query: string) {
   this._ArxivService.buscarArticulos(query).subscribe(
-    (xml: string) => {this.resultado.set(xml);
+    (xml: any) => {
+      console.log(xml)
+      this.resultado.set(xml);
     this.query = '';
     }
   );
--- src/app/articulos/BuscarArticulos/servicios/arxiv-service.ts
+++ src/app/articulos/BuscarArticulos/servicios/arxiv-service.ts
@@ -2,53 +2,58 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../../../enviroments/environment.development';
 
-import { Observable, of } from 'rxjs';
-import { ResultadoConsultaArXiv } from '../interfaces/arxiv-input';
+import { Observable, map } from 'rxjs';
+
+import { ArxivOut, ArxivSuccess } from '../interfaces/arxiv-input';
+import { XMLParser } from 'fast-xml-parser';
+
 @Injectable({
   providedIn: 'root',
 })
 export class ArxivService {
-  _httpClient = inject(HttpClient)
 
-  //Es string debido que ArXiv devuelve un STRING XML 
-  buscarArticulos(query : string): Observable<string>
-  {
+  private httpClient = inject(HttpClient)
+  private parser = new XMLParser()
+
+  buscarArticulos(query: string): Observable<ArxivSuccess> {
+
     const params = new HttpParams()
-    .set('search_query', `all:${query}`)
-    .set('start', 0)
-    .set('max_results', 5)
-    .set('sortBy', "relevance")
-    .set('sortOrder', 'ascending')
-    
-    return this._httpClient.get(environment.arxivQueryUrl, {params, responseType: 'text'})
-  }
+      .set('search_query', `all:${query}`)
+      .set('start', 0)
+      .set('max_results', 10)
+      .set('sortBy', 'relevance')
+      .set('sortOrder', 'ascending')
 
-  /*
-  parsearArxiv(xml : string) : ResultadoConsultaArXiv
-  {
-    if (xml.length == 0){
+    return this.httpClient
+  .get(environment.arxivQueryUrl, { params, responseType: 'text' }).pipe(
+    map(xml => {
+
+      let parsed = this.parser.parse(xml)
+
+      let entries_xml = parsed.feed.entry
+      let entries: ArxivOut[] = entries_xml.map((entries: any) => {
+
+        let authors = Array.isArray(entries.author)
+          ? entries.author.map((a: any) => a.name)
+          : [entries.author?.name]
+          
+        return {
+          id: entries.id,
+          title: entries.title,
+          summary: entries.summary,
+          authors,
+          published: entries.published,
+          updated: entries.updated
+        }
+
+      })
+
       return {
-        Response : true,
-        entries : []
-      }
-    
-    }else if (xml.length != 0) {
-      return {
-        Response : true,
-        entries : []
+        Response: true,
+        entries
       }
 
-    }else {
-      return {
-        Response : false,
-        Error : "Not Found the Article"
-      }
-    }
-    }
-  resultadoBusquedaArticulo( query: string) : Observable<ResultadoConsultaArXiv>{
-    this.buscarArticulos(query).pipe(map(response => ResultadoConsultaArXiv))
-    
-  }
-
- */ 
+    })
+  )}
 }
+//pipe sirve para encadenar operadores que transforman los datos que pasan por un Observable.
