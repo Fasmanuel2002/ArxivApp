1b7dcb509d4e1dd86272b7953fb14880f463e698
ManuelFas <manuelfasruiz@gmail.com>
2026-03-23 10:03:02 +0100

    Working favoritos

--- src/app/articulos/ArticulosFavoritos/favoritos-service.ts
+++ src/app/articulos/ArticulosFavoritos/favoritos-service.ts
@@ -1,20 +1,24 @@
 import { effect, Injectable, signal } from '@angular/core';
 import { environment } from '../../../enviroments/environment.development';
 import { ArxivOut, ArxivSuccess } from '../BuscarArticulos/interfaces/arxiv-input';
-
+import { isPlatformBrowser } from '@angular/common';
+import { inject, PLATFORM_ID } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FavoritosService {
+  _platformId = inject(PLATFORM_ID);
   ArticulosFavoritos = signal<ArxivOut[]> (this.cargarArticulos())
 
   private cargarArticulos(){
+    if (!isPlatformBrowser(this._platformId)) return; 
     const articulos = localStorage.getItem(environment.STORAGE_KEY)
     return articulos ? JSON.parse(articulos) : []
   }
 
   guardarArticulos = effect(() => {
+    if (!isPlatformBrowser(this._platformId)) return;
     const articulos_favoritos = this.ArticulosFavoritos()
     localStorage.setItem(environment.STORAGE_KEY, JSON.stringify(articulos_favoritos))
     console.log("Se ha actualizado el LocalStorage")
